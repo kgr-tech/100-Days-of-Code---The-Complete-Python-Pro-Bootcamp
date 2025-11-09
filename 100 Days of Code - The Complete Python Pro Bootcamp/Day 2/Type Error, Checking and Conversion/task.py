74e19b64e32b6811454f44ae97d1ee76c5b0f8c6
kgr-tech <giresh30reddy@gmail.com>
2025-11-09 14:41:55 +0530

    second change

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
@@ -6,7 +6,7 @@ print(type(78.578))
 print(type(4567))
 print(type(False))
 
-print( int("678") +int("897"))
+print( float("89.90") +int("897"))
 print(float("78.90") + float("896.7"))
 
 print(len("Number of letters in your name: ") + len(input("Enter your name : \n")))
